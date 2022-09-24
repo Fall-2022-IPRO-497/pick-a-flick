7cfe93c73c2a2ef2103b6967e176ffeec418b83e
Yongkang Hu <hu.yongkangsg114@gmail.com>
2022-09-23 22:44:26 -0500

    create express server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
-console.log("test");
+const express = require('express')
+const app = express()
+app.use(express.json())
+var morgan = require('morgan')
+app.use(morgan('tiny'))
+const cors = require('cors')
+app.use(cors())
+require('dotenv').config()
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
