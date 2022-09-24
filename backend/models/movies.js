09009aaa2936e45e94a3416e2443c7db9224e74e
Yongkang Hu <hu.yongkangsg114@gmail.com>
2022-09-23 22:50:44 -0500

    add basic movie schema

---
+++ backend/models/movies.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url)
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch(error => {
+    console.log(error.message)
+  })
+
+  const movieSchema = new mongoose.Schema({
+    'movieName': {
+      type: String
+    },
+  })
+
+  movieSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+
+  module.exports = mongoose.model('Movie', movieSchema)
