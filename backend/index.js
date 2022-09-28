4f1ef2325e1025517ee97af68e3403b624cb4ef0
Yongkang Hu <hu.yongkangsg114@gmail.com>
2022-09-27 19:35:01 -0500

    add update, delete, and post routes

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,30 @@ app.get('/api/movies', (req, res) => {
       .catch(error => console.log(error))
 })
 
+app.post('/api/movies/', (req, res) => {
+    const movie = new Movie({
+        movieName: "Test"
+    })
+    movie.save()
+        .then(savedMovie => {
+            res.json(savedMovie)
+        })
+        .catch(error => console.log(error))
+})
+
+app.delete('/api/movies/:id', (req, res) => {
+    console.log(req.params.id)
+    Movie.findByIdAndRemove(req.params.id)
+        .then(() => {
+            res.status(204).end()
+        })
+        .catch(error => console.log(error))
+})
+
+app.put('/api/movies/:id', (req, res) => {
+    console.log("Update: need to know schema before adding actual logic")
+})
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
