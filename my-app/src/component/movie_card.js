98dc16926d6627823e593079e3e4ac80aaf1ac13
thartley56 <74573560+thartley56@users.noreply.github.com>
2022-11-01 20:52:14 -0500

    added function/component for rendering movie details by id, also deleted un-needed files

--- my-app/src/component/movie_card.js
+++ my-app/src/component/movie_card.js
@@ -1,21 +1,35 @@
 import moviePosterEx from './RevengeOfTheSith.jpg';
 import './movie_card.css';
 import Image from 'react-bootstrap/Image'
+import PropTypes from 'prop-types';
+import MovieDetails from '../get_movie_details';
 
 
 function MovieCard() {
 
-    return (
-    <div className='movie-card-container'>
-        <div >
-            <Image className='movie-card-image' src={moviePosterEx} fluid={true} ></Image>
-            <h1>Star Wars: Episode III - Revenge of the Sith</h1>
-            <h2>Year Released: 2005 </h2>
-            <h2>Genre: Action/Sci-Fi</h2>
-            <h3>Run Time: 2h 20m</h3>
-        </div>
+    var test = [354912, 760161, 436270, 642885, 616820, 718930, 916605]
 
-    </div>);
+    if (1 == 0){
+        return (
+            <div className='movie-card-container'>
+                <div >
+                    <Image className='movie-card-image' src={moviePosterEx} fluid={true} ></Image>
+                    <h1>Star Wars: Episode III - Revenge of the Sith</h1>
+                    <h2>Year Released: 2005 </h2>
+                    <h2>Genre: Action/Sci-Fi</h2>
+                    <h3>Run Time: 2h 20m</h3>
+                </div>
+            </div>
+        );
+    }
+    else {
+        return (
+            <div>
+                <MovieDetails id = {test[0]}/>
+            </div>
+        );
+    }
+    
 }
 
 export default MovieCard;
--- my-app/src/component/recommendations_page.js
+++ my-app/src/component/recommendations_page.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './recommendations_page.css'
-import RecTable from './rec_table';
+import PopularMoviesList from '../popular_movies_list'
 
 
  function Recommendations(){
     return (
         <div>
             <h1>Your Movie Recommendations</h1>
-            <RecTable/>
+            <PopularMoviesList/>
         </div>
     );
 }
