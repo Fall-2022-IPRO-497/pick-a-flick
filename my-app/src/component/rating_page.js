8a3d18dd8680475214ff521cc9b366057a4aa728
MonkeyDeCoo <mariohzhang@gmail.com>
2022-11-08 18:00:42 -0600

    refactor

--- my-app/src/component/rating_page.js
+++ my-app/src/component/rating_page.js
@@ -5,19 +5,15 @@ import './rating_page.css'
 import thumbsUp from './thumbs-up.png';
 import thumbsDown from './thumbs-down.png';
 import Image from 'react-bootstrap/Image'
-import {click_like} from './Like_button.js'
-import {click_dislike} from './Dislike_button.js'
-import {click_neverseen} from './Neverseen_button.js'
 
- function Rating({userDetails}){
-    console.log("in rating->", userDetails)
+ function Rating({userDetails, updateMovieRating}){
     return (
     <div>
         <MovieCard></MovieCard>
-        <div>
-          <Button variant="success" ><Image className='thumb-button' src = {thumbsUp} onClick = {event => click_like(event, userDetails)} fluid={true}></Image></Button>{' '}
-          <Button variant="secondary" onClick = {event => click_neverseen(event, userDetails)}>Havent Seen It</Button>{' '}
-          <Button variant="danger" ><Image  className='thumb-button' src = {thumbsDown} onClick = {event => click_dislike(event, userDetails)}fluid={true}></Image></Button>{' '}
+        <div classname = 'button-container'>
+          <Button variant="success" ><Image className='thumb-button' src = {thumbsUp} onClick = {event => updateMovieRating(event, userDetails, "like")} fluid={true}></Image></Button>{' '}
+          <Button variant="secondary" onClick = {event => updateMovieRating(event, userDetails, "unwatched")}>Havent Seen It</Button>{' '}
+          <Button variant="danger" ><Image  className='thumb-button' src = {thumbsDown} onClick = {event => updateMovieRating(event, userDetails, "dislike")}fluid={true}></Image></Button>{' '}
         </div>
     </div>
     );
